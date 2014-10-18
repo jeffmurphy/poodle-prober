1d55a4875ece1b5f2221c328cfa456621c20210e
kylekoza <kyle.koza@gatech.edu>
2014-10-18 00:39:48 -0400

    Added ability to scan multiple subnets

--- sslv3check.py
+++ sslv3check.py
@@ -7,91 +7,78 @@ python3 sslcheck.py 10.0.1.0/24
 
 jcmurphy@buffalo.edu
 """
-import socket, ssl, pprint, sys, IPy, getopt
+import socket, ssl, pprint, sys, IPy, argparse
 
-def help(m=""):
-	print("sslv3check.py [-p port,port,...] [-n <network/mask> OR -H hostname] [-t]")
-	print("   -p port to connect to (default=443)")
-	print("   -t check if SSLv3 is enabled and TLSv1 is not enabled")
-	print("      otherwise just see if SSLv3 is enabled")
-	print(m)
-	sys.exit(2)
+parser = argparse.ArgumentParser(description='Scan a netblock for SSLv3 enabled servers on port 443')
+parser.add_argument('--port', '-p', nargs='*', default="443", help='port to connect to (default=443)')
+parser.add_argument('--network', '-n', nargs='*', default=None, help='<network/mask>')
+parser.add_argument('--host', '-H', nargs='*', default=None, help='hostname')
+parser.add_argument('--tls', '-t', action='store_true', default=False, help='check if SSLv4 is enabled and TLSv1 is not enabled\n otherwise just see if SSLv3 is enabled')
+
 
 def print_results(host, port, sslv3, tlsv1):
-	if tlsv1 is None:
-		print("{0}:{1} SSLv3 {2}".format(str(host), port, sslv3))
-		return
+    if tlsv1 is None:
+        print("{0}:{1} SSLv3 {2}".format(str(host), port, sslv3))
+        return
 
-	if sslv3 == "enabled" and tlsv1 != "enabled":
-		print("{0}:{1} SSLv3 enabled and TLSv1 not enabled".format(str(host), port))
-	else:
-		print("{0}:{1} SSLv3={2} TLSv1={3}".format(str(host), port, sslv3, tlsv1))
+    if sslv3 == "enabled" and tlsv1 != "enabled":
+        print("{0}:{1} SSLv3 enabled and TLSv1 not enabled".format(str(host), port))
+    else:
+        print("{0}:{1} SSLv3={2} TLSv1={3}".format(str(host), port, sslv3, tlsv1))
 
 def main():
-	port = "443"
-	try:
-		opts, args = getopt.getopt(sys.argv[1:], "hp:n:H:t")
-	except getopt.GetoptError:
-		help()
+    args = parser.parse_args()
+    args = vars(args)
 
-	network = host = tlsv1 = None
-	no_tlsv1 = False
+    network = host = tlsv1 = None
+    no_tlsv1 = False
 
-	for opt, arg in opts:
-		if opt == '-h':
-			help()
-		elif opt == '-n':
-			network = arg
-		elif opt == '-p':
-			port = arg
-		elif opt == '-t':
-			no_tlsv1 = True
-		elif opt == '-H':
-			host = arg
-	
-	if network == None and host == None:
-		help("-n or -H required")
+    if args["network"] == None and host == None:
+            help("-n or -H required")
 
-	if host is not None:
-		for p in port.split(","):
-			sslv3 = check_sslv3(host, p)
-			if no_tlsv1 == True:
-				tlsv1 = check_tls(host, p)
-			print_results(host, p, sslv3, tlsv1)
-		return
+    if args["host"] is not None:
+        for host in args["host"]:
+            for p in args["port"]:
+                sslv3 = check_sslv3(host, p)
+                if tls == True:
+                    tlsv1 = check_tls(host, p)
+                print_results(host, p, sslv3, tlsv1)
+        return
 
-
-	ip = IPy.IP(network)
-
-	for x in ip:
-		if ip.prefixlen() != 32 and (ip.broadcast() == x or ip.net() == x):
-			continue
-		for p in port.split(","):
-			sslv3 = check_sslv3(x, p)
-			if no_tlsv1 == True:
-				tlsv1 = check_tls(x, p)
-			print_results(x, p, sslv3, tlsv1)
+    net = IPy.IPSet()
+    for network in args["network"]:
+        net.add(IPy.IP(network))
+    
+    for ip in net:
+        for x in ip:
+            if ip.prefixlen() != 32 and (ip.broadcast() == x or ip.net() == x):
+                continue
+            for p in args["port"]:
+                sslv3 = check_sslv3(x, p)
+                if no_tlsv1 == True:
+                        tlsv1 = check_tls(x, p)
+                print_results(x, p, sslv3, tlsv1)
 
 
 def check_tls(h, p):
-	return check(h, p, ssl.PROTOCOL_TLSv1)
+    return check(h, p, ssl.PROTOCOL_TLSv1)
 
 def check_sslv3(h, p):
-	return check(h, p, ssl.PROTOCOL_SSLv3)
+    return check(h, p, ssl.PROTOCOL_SSLv3)
 
 def check(h, p, ctx):
-	context = ssl.SSLContext(ctx)
-	context.verify_mode = ssl.CERT_NONE
+    context = ssl.SSLContext(ctx)
+    context.verify_mode = ssl.CERT_NONE
 
-	try:
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.settimeout(1)
-		ssl_sock = context.wrap_socket(s, server_hostname=str(h), do_handshake_on_connect=True)
-		ssl_sock.connect((str(h), int(p)))
-		ssl_sock.close()
-		return "enabled"
-	except Exception as e:
-		return str(e)
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)
+        ssl_sock = context.wrap_socket(s, server_hostname=str(h), do_handshake_on_connect=True)
+        ssl_sock.connect((str(h), int(p)))
+        ssl_sock.close()
+        return "enabled"
+    except Exception as e:
+        return str(e)
 
 if __name__ == "__main__":
-	main()
+        main()
